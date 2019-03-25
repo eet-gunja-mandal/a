aaf73f186f02be81f5df7af9969f82a7e159023c
gunjaent <gunja@e-et-t.com>
2019-03-25 14:33:55 +0545

    random file is added

---
+++ random/random.cpp
@@ -0,0 +1,17 @@
+// random.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x = 10, y = 8;
+		int dist;
+		dist = (x - y);
+
+		cout << " the distance between two point is:" << dist<<endl;
+	system("pause");
+    return 0;
+}
+
